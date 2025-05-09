aa1045c56c8a98c7b0093d696706ad117463ca33
Red0ak97~ <dk08082017@gmail.com>
2025-05-09 23:41:00 +0300

    add taskThisMethod.js

---
+++ taskThisMethod.js
@@ -0,0 +1,21 @@
+//Измените код методов up, down и showStep таким образом, чтобы их вызов можно было сделать по цепочке, например так://
+
+
+
+let ladder = {
+    step: 0,
+    up() {
+      this.step++;
+      return this;
+    },
+    down() {
+      this.step--;
+      return this;
+    },
+    showStep() {
+      alert( this.step );
+      return this;
+    }
+  };
+  
+  ladder.up().up().down().showStep().down().showStep(); // показывает 1 затем 0
